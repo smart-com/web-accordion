9cd7b454afe9ac8f63e7c288df23bd9cea29ea2a
smart-com <rikhnavskaja.elena@gmail.com>
2016-07-18 13:25:44 +0200

    Shadow Dom is available from accordion.js

--- accordion.js
+++ accordion.js
@@ -38,30 +38,32 @@
     
     return registerElem( customTag, doc, parentPrototype );
   };
-   
-  // this - Создаваемый HTML-элемент
-  function createShadowRoot() {
-    // Находим шаблон по имени тега, 
-    // потому что идентификатор трудно передать в параметры
-    var templates = localDocument.getElementsByTagName( 'template' );
-    var tmpl = templates[ 0 ];
-    var root = this.createShadowRoot();
-    root.appendChild( tmpl.content.cloneNode( true ) );
-  }
+  
+  // Получаем хост-элемент из index.html
+  var host = $('#host');
+  // Создаем в хосте теневой дом
+  var shadowRoot = host[ 0 ].createShadowRoot();
+  // Получаем шаблон из accordion.html
+  var tpl = localDocument.querySelector('#accordion-tpl');
+  // Вставляем его содержимое в теневой дом
+  shadowRoot.appendChild( tpl.content );
+  // Обязательно удаляем шаблон после использования
+  tpl.remove();
+  
+  // Получаем аккордеон из теневого дома
+  var accordion = $( 'body /deep/ #accordion' );
+  console.log( accordion );
   
   // Регистрируем новый элемент в браузере
-  var AccordionProto = registerElem( "web-accordion", localDocument,  HTMLDivElement.prototype, createShadowRoot );
+  var AccordionProto = registerElem( 
+    "web-accordion", localDocument, HTMLDivElement.prototype );
+  
   // Создаем элемент
   var accordion = new AccordionProto();
-  console.log( accordion );
-  // Помещаем его в body
-  document.body.appendChild( accordion );
   
   // Здесь будет API
   // this - HTML-элемент
   AccordionProto.prototype.doAnything = function() {
-    console.log('something matter...');
-    //console.log( this );
   };
   
   // Точно так же регистрируем остальные элементы
@@ -74,12 +76,10 @@
   
   // Создаем заголовок
   var title = new HeaderProto();
-  console.log( title );
   
   // Здесь будет API
   // this - HTML-элемент
   HeaderProto.prototype.doAnything = function() {
-    this.style.backgroundColor = 'green';
   };
   
   // Запускаем функцию
